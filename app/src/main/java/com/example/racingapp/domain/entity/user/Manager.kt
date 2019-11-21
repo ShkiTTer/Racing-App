6e8bbe1373283ea7b0f54930b55795237f56cae7
ShkiTTer <shkipper65@gmail.com>
2019-11-21 13:17:09 +0700

    Add team property to manager

--- app/src/main/java/com/example/racingapp/domain/entity/user/Manager.kt
+++ app/src/main/java/com/example/racingapp/domain/entity/user/Manager.kt
@@ -1,9 +1,12 @@
 package com.example.racingapp.domain.entity.user
 
+import com.example.racingapp.domain.entity.Team
+
 data class Manager(
     override val login: String,
     override val password: String,
     val name: String,
     val secondName: String,
+    val team: Team,
     override val role: UserRole = UserRole.MANAGER
 ): User()
