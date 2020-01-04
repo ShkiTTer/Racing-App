498e405620b3e7d71ec5b483d1d87d045508f7f6
ShkiTTer <shkipper65@gmail.com>
2020-01-04 15:29:44 +0700

    Add track adapter

--- app/src/main/java/com/example/racingapp/domain/entity/Track.kt
+++ app/src/main/java/com/example/racingapp/domain/entity/Track.kt
@@ -1,10 +1,7 @@
 package com.example.racingapp.domain.entity
 
-import androidx.annotation.DrawableRes
-
 data class Track(
     val title: String,
     val country: String,
-    val length: Int, // In meters
-    @DrawableRes val scheme: Int
+    val length: Int // In meters
 )
---
+++ app/src/main/java/com/example/racingapp/presentation/adapter/TrackAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.racingapp.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.racingapp.R
+import com.example.racingapp.domain.entity.Track
+import com.example.racingapp.presentation.adapter.common.BaseAdapter
+import com.example.racingapp.presentation.adapter.common.ViewHolder
+import kotlinx.android.synthetic.main.item_track.view.*
+
+class TrackAdapter : BaseAdapter<Track, TrackAdapter.TrackViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder =
+        TrackViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_track,
+                parent,
+                false
+            )
+        )
+
+    class TrackViewHolder(v: View) : ViewHolder<Track>(v) {
+        private val title = v.tvTitle
+        private val country = v.tvCountry
+        private val length = v.tvLength
+
+        override fun bind(item: Track) {
+            title.text = item.title
+            country.text = item.country
+            length.text = "${item.length}m"
+        }
+    }
+}
